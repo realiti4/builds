e6efb0dce5462c88682f06770dca7c587ec9a797
realiti4 <onurcetinkol@gmail.com>
2020-02-04 15:17:52 +0300

    removed compiler as it is fixed

--- utils/env_vars.bat
+++ utils/env_vars.bat
@@ -5,7 +5,7 @@ set CMAKE_GENERATOR=Ninja
 
 set BUILD_TEST=0
 
-if not exist \magma (
+if not exist magma\ (
 	curl -k https://s3.amazonaws.com/ossci-windows/magma_2.5.1_%CUDA_PREFIX%_%CONFIG%.7z -o magma.7z
 	7za x -aoa magma.7z -omagma
 	del magma_2.5.1_%CUDA_PREFIX%_%CONFIG%.7z
@@ -15,8 +15,5 @@ set MKLProductDIR=D:\Files\Program\IntelSWTools\compilers_and_libraries_2020.0.1
 
 set LIB=%MKLProductDir%\mkl\lib;%LIB%
 set LIB=%MKLProductDir%\mkl\lib\intel64_win;%LIB%
-set LIB=%MKLProductDir%\compiler\lib\intel64_win;%LIB%
 set CMAKE_INCLUDE_PATH=%MKLProductDIR%\mkl\include;%CMAKE_INCLUDE_PATH%
 set MAGMA_HOME=%cd%\magma
-
-echo "done!"
