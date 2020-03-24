84d83150adbd3ef84bf246650222f8d11be013c3
realiti4 <onurcetinkol@gmail.com>
2020-03-24 18:15:55 +0300

    updated magma to 2.5.2

--- utils/env_vars.bat
+++ utils/env_vars.bat
@@ -6,9 +6,9 @@ set CMAKE_GENERATOR=Ninja
 set BUILD_TEST=0
 
 if not exist magma\ (
-	curl -k https://s3.amazonaws.com/ossci-windows/magma_2.5.1_%CUDA_PREFIX%_%CONFIG%.7z -o magma.7z
+	curl -k https://s3.amazonaws.com/ossci-windows/magma_2.5.2_%CUDA_PREFIX%_%CONFIG%.7z -o magma.7z
 	7za x -aoa magma.7z -omagma
-	del magma_2.5.1_%CUDA_PREFIX%_%CONFIG%.7z
+	del magma_2.5.2_%CUDA_PREFIX%_%CONFIG%.7z
 )
 
 set MKLProductDIR=D:\Files\Program\IntelSWTools\compilers_and_libraries_2020.0.166\windows
