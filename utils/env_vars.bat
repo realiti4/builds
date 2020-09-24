8ebaa42dc5ef11e98fba3fb339fb438f232a1446
realiti4 <onurcetinkol@gmail.com>
2020-09-24 22:05:55 +0300

    env vars v2

--- utils/env_vars.bat
+++ utils/env_vars.bat
@@ -1,17 +1,21 @@
-set CUDA_PREFIX=cuda102
 set CONFIG=release
 
 set CMAKE_GENERATOR=Ninja
 
 set BUILD_TEST=0
 
+set CUDA_PREFIX=cuda102
+set MAGMA_VERSION=2.5.3
+
 if not exist magma\ (
-	curl -k https://s3.amazonaws.com/ossci-windows/magma_2.5.2_%CUDA_PREFIX%_%CONFIG%.7z -o magma.7z
+	curl -k https://s3.amazonaws.com/ossci-windows/magma_%MAGMA_VERSION%_%CUDA_PREFIX%_%CONFIG%.7z -o magma.7z
 	7za x -aoa magma.7z -omagma
 	del magma.7z
 )
 
-set MKLProductDIR=D:\Files\Program\IntelSWTools\compilers_and_libraries_2020.1.216\windows
+set DISTUTILS_USE_SDK=1
+
+set MKLProductDIR=D:\Files\Program\IntelSWTools\compilers_and_libraries_2020.2.254\windows
 
 set LIB=%MKLProductDir%\mkl\lib;%LIB%
 set LIB=%MKLProductDir%\mkl\lib\intel64_win;%LIB%
